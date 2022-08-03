9ca1b8b3a3be528394d906767b66614e2eca8d03
HK-Ibrahim92 <syedzada786alam@gmail.com>
2022-08-03 08:50:07 +0500

    Date Methods

---
+++ app..js
@@ -0,0 +1,25 @@
+var now =new Date()
+
+ document.write(now+"<br>")
+ now.setDate(13)
+ document.write(now+"<br>")
+ now.setHours(4)
+ document.write(now)
+// document.write(now.toDateString()+"<br>")
+
+// document.write(now.getDate()+"<br>")
+
+// document.write(now.getFullYear()+"<br>")
+
+// document.write(now.getMonth()+"<br>")
+
+// document.write(now.getDay()+"<br>")
+
+// document.write(now.getHours()+"<br>")
+
+// document.write(now.getMinutes()+"<br>")
+
+// document.write(now.getSeconds()+"<br>")
+
+// document.write(now.getMilliseconds()+"<br>")
+
